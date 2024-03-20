5e904d17887dde18711ea7bfe3b7657a8816c63a
MeiMeitonymie <meipalanque@yahoo.com>
2024-03-20 10:13:07 +0100

    Préparation pour P9

--- examples/astro/solve_astro_clump.py
+++ examples/astro/solve_astro_clump.py
@@ -65,9 +65,9 @@ def get_dim_coeff(
 
 if __name__ == "__main__":
     # Model
-    use_m1 = True
-    use_pn = False
-    pn_order = 5
+    use_m1 = False
+    use_pn = True
+    pn_order = 9
 
 
     # Adim values
